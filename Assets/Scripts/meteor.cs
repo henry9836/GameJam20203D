55f8981f39811b994d4de54d286e1e623e0fadf1
vaughan <vaughanw2012@gmail.com>
2020-02-01 11:16:33 +1300

    metior and mine done

--- Assets/Scripts/meteor.cs
+++ Assets/Scripts/meteor.cs
@@ -15,6 +15,10 @@ public class meteor : MonoBehaviour
     public GameObject meteorobj;
     public float hitspred = 0.9f;
 
+    private float lifetime = 30.0f;
+
+    public float HP = 100.0f;
+
     void Start()
     {
         Debug.Log("hi");
@@ -28,10 +32,18 @@ public class meteor : MonoBehaviour
     {
         this.transform.localScale = new Vector3(size, size, size);
         this.gameObject.GetComponent<Rigidbody>().mass = 150.0f;
+
+        lifetime -= Time.deltaTime;
+
+        if (lifetime <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public void isshot()
     {
+
         GetComponent<SphereCollider>().enabled = false;
         GetComponent<MeshRenderer>().enabled = false;
         Destroy(GetComponent<Rigidbody>());
@@ -39,12 +51,14 @@ public class meteor : MonoBehaviour
         GameObject meteorobj1 = Instantiate(meteorobj, transform.position, transform.rotation);
         meteorobj1.GetComponent<meteor>().isSubDivide = true;
         meteorobj1.GetComponent<meteor>().size = size * 0.65f;
+        meteorobj1.GetComponent<meteor>().HP = 100.0f;
         meteorobj1.GetComponent<SphereCollider>().enabled = true;
         meteorobj1.GetComponent<MeshRenderer>().enabled = true;
 
         GameObject meteorobj2 = Instantiate(meteorobj, transform.position, transform.rotation);
         meteorobj2.GetComponent<meteor>().isSubDivide = true;
         meteorobj2.GetComponent<meteor>().size = size * 0.65f;
+        meteorobj2.GetComponent<meteor>().HP = 100.0f;
         meteorobj2.GetComponent<SphereCollider>().enabled = true;
         meteorobj2.GetComponent<MeshRenderer>().enabled = true;
 
@@ -79,10 +93,8 @@ public class meteor : MonoBehaviour
                 size = Mathf.Lerp(0.0f, initsize, t);
                 yield return null;
             }
-
             Vector3 dir = -(this.transform.position - player.transform.position).normalized;
             this.gameObject.GetComponent<Rigidbody>().AddForce(dir * speed, ForceMode.Impulse);
-
         }
         else 
         {
@@ -90,9 +102,9 @@ public class meteor : MonoBehaviour
             float yrand = Random.Range(hitspred, -hitspred);
             float xrand = Random.Range(Mathf.Sqrt(Mathf.Pow(hitspred, 2) - Mathf.Pow(yrand, 2)), -Mathf.Sqrt(Mathf.Pow(hitspred, 2) - Mathf.Pow(yrand, 2)));
             Vector3 vec3dir = new Vector3(xrand, yrand, 1);
-            this.gameObject.GetComponent<Rigidbody>().AddForce(-vec3dir * speed, ForceMode.Impulse);
+            vec3dir = this.transform.rotation * vec3dir;
 
-
+            this.gameObject.GetComponent<Rigidbody>().AddForce(vec3dir * speed, ForceMode.Impulse);
         }
     }
 }
--- Assets/Scripts/mineable.cs
+++ Assets/Scripts/mineable.cs
@@ -6,4 +6,7 @@ public class mineable : MonoBehaviour
 {
    
     public Inventory.ITEM selected;
+
+    public float HP = 100.0f;
 }
+
