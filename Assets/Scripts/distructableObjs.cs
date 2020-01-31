9b96a3de78831592343ec00cd7b771b4e3134753
vaughan <vaughanw2012@gmail.com>
2020-01-31 23:01:18 +1300

    meteror collisioon works on distructable objects

---
+++ Assets/Scripts/distructableObjs.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class distructableObjs : MonoBehaviour
+{
+    public float HP = 100.0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/meteor.cs
+++ Assets/Scripts/meteor.cs
@@ -5,33 +5,40 @@ using UnityEngine;
 public class meteor : MonoBehaviour
 {
     public float size = 1.0f;
+    public float additionalexplosionraduis = 1.0f;
+    public LayerMask distructable;
     public float initsize = 3.0f;
     public float speed = 1.0f;
     public GameObject player;
-    public bool collisionbool = false;
+    public bool DeathIsInevitable = false;
+
     
 
     void Start()
     {
         StartCoroutine(grow());
         initsize = Random.Range(2.0f, 4.0f);
-
-
     }
 
     void Update()
     {
         this.transform.localScale = new Vector3(size, size, size);
         this.gameObject.GetComponent<Rigidbody>().mass = ((4.0f / 3.0f) * Mathf.PI * Mathf.Pow(size, 3.0f));
-
-        
     }
 
     void OnCollisionEnter(Collision collision)
     {
-
-
-
+        if (DeathIsInevitable == false)
+        {
+            DeathIsInevitable = true;
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position, size + additionalexplosionraduis, transform.forward, 1.0f, distructable);
+            foreach (var hit in hits)
+            {
+                Debug.Log(hit.transform.gameObject.name);
+                hit.transform.gameObject.GetComponent<distructableObjs>().HP -= size * 25.0f;
+            }
+            Destroy(this.gameObject);
+        }
     }
 
 
