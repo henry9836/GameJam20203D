4023d5db2617f90b012670e95ac8e57418949474
Henry <henry983615@gmail.com>
2020-01-31 22:36:57 +1300

    Player Moves needs jumping

--- Assets/Scripts/player/PlayerMovement.cs
+++ Assets/Scripts/player/PlayerMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float maxSpeed = 50.0f;
+    public float moveSpeed = 10.0f;
+    [Range(0.0f, 0.99f)]
+    public float slowDownRate = 0.5f;
+    public float mathMoveForceAmp = 10.0f;
+
+    private Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveDir = Vector3.zero;
+
+        //Apply Input To Dir Vector
+        moveDir += (transform.forward * Input.GetAxisRaw("Vertical"));
+        moveDir += (transform.right * Input.GetAxisRaw("Horizontal"));
+        moveDir = moveDir.normalized;
+
+        //Slow Down Fast If We Are Not Pressing Anything
+        if (moveDir == Vector3.zero)
+        {
+            //Expect for y
+            rb.velocity = new Vector3(rb.velocity.x * slowDownRate, rb.velocity.y, rb.velocity.z * slowDownRate);
+        }
+
+        else if (rb.velocity.magnitude < maxSpeed)
+        {
+
+            float mathMoveForce = Mathf.Clamp(0.02f + Mathf.Pow(Mathf.Cos((0.5f - (rb.velocity.magnitude / maxSpeed) * Mathf.PI)/2), 0.5f), 0.0f, 1.0f);
+
+            rb.AddForce(moveDir * (moveSpeed * mathMoveForce) * Time.deltaTime);
+        }
+
+
     }
+
 }
