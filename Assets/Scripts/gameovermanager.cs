bdbae2605fcb9b651395e947ec4ad13b38f8ad08
vaughan <vaughanw2012@gmail.com>
2020-02-01 15:28:01 +1300

    slow mo ending

---
+++ Assets/Scripts/gameovermanager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameovermanager : MonoBehaviour
+{
+    public bool gameover = false;
+    public float deathdistance = 60.0f;
+    public float screenshotThreshold = 0.01f;
+    public GameObject player;
+    public GameObject canvas;
+    private float fixedDeltaTime;
+
+    void Awake()
+    {
+        Time.timeScale = 1.0f;
+        Time.fixedDeltaTime = 0.02f;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(player.transform.position, Vector3.zero) >= deathdistance && gameover == false)
+        {
+            player.transform.GetChild(0).GetComponent<MouseLook>().enabled = false;
+            gameover = true;
+            GameObject.Find("Canvas").gameObject.SetActive(false);
+            canvas.gameObject.SetActive(true);
+            canvas.transform.GetChild(0).GetComponent<Text>().text = "Score: " + this.gameObject.GetComponent<score>().thescore.ToString("F0");
+
+            StartCoroutine(slow());
+        }
+    }
+
+    public IEnumerator slow()
+    {
+        for (float t = 0.0f; t < 1.0f; t += Time.unscaledDeltaTime * 0.3f)
+        {
+            Debug.Log(Time.timeScale);
+            Time.timeScale = Mathf.Lerp(1.0f, screenshotThreshold, t);
+            Time.fixedDeltaTime = .02f * Time.timeScale;
+
+            yield return null;
+        }
+
+        //save and freeze game
+        Debug.LogWarning("fddf");
+        Time.timeScale = 0.0f;
+        Time.fixedDeltaTime = 0.0f;
+
+        //take screenshot
+
+        yield return new WaitForSecondsRealtime(0.1f);
+
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+
+
+
+}
