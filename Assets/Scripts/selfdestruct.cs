5946f7a6104e2cf0146e391ca78afbb6658bdd56
vaughan <vaughanw2012@gmail.com>
2020-02-02 08:31:37 +1300

    despawn particle

---
+++ Assets/Scripts/selfdestruct.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selfdestruct : MonoBehaviour
+{
+
+    public float killAfterSeconds = 60.0f;
+
+
+    void Update()
+    {
+        killAfterSeconds -= Time.deltaTime;
+        if (killAfterSeconds <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
