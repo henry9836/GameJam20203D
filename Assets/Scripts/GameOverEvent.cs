71a43e305f9819eba03861c08192cd3601671eb3
Henry <henry983615@gmail.com>
2020-02-01 14:55:12 +1300

    Fixed inconstant screenshots

--- Assets/Scripts/GameOverEvent.cs
+++ Assets/Scripts/GameOverEvent.cs
@@ -9,29 +9,37 @@ public class GameOverEvent : MonoBehaviour
     string pathPrefix = @"file://";
     string filename = @"gameoverscreen.png";
 
-    public void CaptureAndLoadScreenShot()
-    {
-        ScreenCapture.CaptureScreenshot("gameoverscreen.png");
-
-        string path = Directory.GetCurrentDirectory() + "\\";
-        string fullFilename = pathPrefix + path + filename;
-
-        Debug.Log(fullFilename);
-
-        WWW www = new WWW(fullFilename);
-        Texture2D screenshot = new Texture2D(1024, 1024, TextureFormat.DXT1, false);
-        www.LoadImageIntoTexture(screenshot);
-
-        GameObject.FindGameObjectWithTag("GAMEOVERSCREEN").GetComponent<MeshRenderer>().material.SetTexture("Texture2D_56079B38", screenshot);
-
-    }
+    bool IOLOCK = false;
 
     private void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Backspace))
+        if (Input.GetKeyDown(KeyCode.E) && !IOLOCK)
         {
-            CaptureAndLoadScreenShot();
+            StartCoroutine(CaptureAndLoadScreenShot());
         }
+    }
+
+    IEnumerator CaptureAndLoadScreenShot()
+    {
+        IOLOCK = true;
+
+        ScreenCapture.CaptureScreenshot("gameoverscreen.png");
+
+        string path = Directory.GetCurrentDirectory() + "\\Meteor Storm_Data\\";
+        string fullFilename = pathPrefix + path + filename;
+
+        Debug.LogError(fullFilename);
+
+        //Wait for IO
+        yield return new WaitForSeconds(1.0f);
+
+        WWW www = new WWW(fullFilename);
+        Texture2D screenshot = new Texture2D(1920, 1080, TextureFormat.DXT1, false);
+        www.LoadImageIntoTexture(screenshot);
+        GameObject.FindGameObjectWithTag("GAMEOVERSCREEN").GetComponent<MeshRenderer>().material.SetTexture("Texture2D_56079B38", screenshot);
+        IOLOCK = false;
+        
+        yield return null;
     }
 
 }
