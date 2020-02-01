df4c74b756f20bad63cf1041c23928234b672c71
Henry <henry983615@gmail.com>
2020-02-01 13:32:30 +1300

    Screenshot works

---
+++ Assets/Scripts/GameOverEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverEvent : MonoBehaviour
+{
+    public void CaptureAndLoadScreenShot()
+    {
+        Debug.Log("Capturing Screenshot...");
+        ScreenCapture.CaptureScreenshot("gameoverscreen.png");
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            CaptureAndLoadScreenShot();
+        }
+    }
+
+}
